14ac0204f21c84b59161580c2c82e3f34d0f5cab
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2019-04-14 12:00:08 +0100

    Create choco-run.ps1

---
+++ choco-run.ps1
@@ -0,0 +1,70 @@
+# Run this first as admin: set-executionpolicy remotesigned
+
+if (Get-Command choco.exe -ErrorAction SilentlyContinue) {
+
+    Write-Host ">>>> enable auto confirmation"
+    & choco feature enable -n allowGlobalConfirmation
+
+    Write-Host ">>>> install: google chrome"
+    & choco upgrade googlechrome
+
+    Write-Host ">>>> install: git"
+    & choco upgrade git.install
+
+    Write-Host ">>>> configure: git"
+    & git config --global user.name "Luiz Felipe Machado da Silva"
+    & git config --global user.email "lfmachadodasilva@gmail.com"
+
+    Write-Host ">>>> install: dotnet core"
+    & choco upgrade dotnetcore
+
+    Write-Host ">>>> install: visual studio code"
+    & choco upgrade visualstudiocode
+
+    Write-Host ">>>> configure: visual studio code"
+    & code --install-extension ms-vscode.csharp
+    & code --install-extension donjayamanne.githistory
+    & code --install-extension peterjausovec.vscode-docker
+    & code --install-extension msjsdiag.debugger-for-chrome
+    & code --install-extension mccarter.start-git-bash
+
+    Write-Host ">>>> install: visual studio 2017 community"
+    #& choco install visualstudio2017community
+    & choco upgrade visualstudio2017community --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
+
+    Write-Host ">>>> install: nodejs"
+    & choco upgrade nodejs
+
+    Write-Host ">>>> install: angular"
+    & npm install -g @angular/cli
+
+    Write-Host ">>>> install: opencover"
+    & choco upgrade opencover
+
+    Write-Host ">>>> install: cmder"
+    & choco upgrade cmder
+
+    Write-Host ">>>> install: postman"
+    & choco upgrade postman
+
+    Write-Host ">>>> install: pgadmin"
+    & choco upgrade pgadmin4
+
+    Write-Host ">>>> install: docker"
+    & choco upgrade docker
+
+    Write-Host ">>>> install: gimp"
+    & choco upgrade gimp
+
+    Write-Host ">>>> install: skype"
+    & choco upgrade skype
+
+    & mkdir c:\\luizfelipe
+    & cd c:\\luizfelipe
+    & git clone https://github.com/lfmachadodasilva/MyExpenses.git
+}
+else {
+    Write-Host ">>>>download and install chocolatey"
+    & Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
