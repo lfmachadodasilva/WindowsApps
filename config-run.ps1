4e640d3d3e32a39eaa6b4bec8bb037124da4615d
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2019-04-15 08:13:10 +0100

    Create config-run.ps1

---
+++ config-run.ps1
@@ -0,0 +1,19 @@
+Write-Host ">>>> configure: visual studio code"
+& code --install-extension ms-vscode.csharp
+& code --install-extension donjayamanne.githistory
+& code --install-extension peterjausovec.vscode-docker
+& code --install-extension msjsdiag.debugger-for-chrome
+& code --install-extension mccarter.start-git-bash
+
+Write-Host ">>>> configure: git"
+& git config --global user.name "Luiz Felipe Machado da Silva"
+& git config --global user.email "lfmachadodasilva@gmail.com"
+
+& mkdir c:\\luizfelipe
+& cd c:\\luizfelipe
+
+Write-Host ">>>> install: myexpenses"
+& git clone https://github.com/lfmachadodasilva/MyExpenses.git
+
+Write-Host ">>>> install: config"
+& git clone https://github.com/lfmachadodasilva/config.git
