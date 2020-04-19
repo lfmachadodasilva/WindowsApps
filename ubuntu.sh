6ad8ec2c7f3dee8ba2c6e1b983100a9625c2fd47
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2020-04-19 14:14:59 +0100

    save git credential

--- ubuntu.sh
+++ ubuntu.sh
@@ -22,25 +22,26 @@ sudo snap install spotify &&
 
 ## update system ##
 
-sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y &&
+sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y
 
 ## configuration ##
 
-gsettings set org.gnome.desktop.interface enable-animations false &&
-gnome-extensions enable  pixel-saver@deadalnix.me &&
+gsettings set org.gnome.desktop.interface enable-animations false
+gnome-extensions enable  pixel-saver@deadalnix.me
 
-git config --global user.name "Luiz Felipe Machado da Silva" &&
-git config --global user.email "lfmachadodasilva@gmail.com" &&
+git config --global user.name "Luiz Felipe Machado da Silva" 
+git config --global user.email "lfmachadodasilva@gmail.com"
+git config credential.helper store
 
-sudo snap alias dotnet-sdk.dotnet dotnet &&
+sudo snap alias dotnet-sdk.dotnet dotnet
 
-code --install-extension ms-dotnettools.csharp &&
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin &&
-code --install-extension dbaeumer.vscode-eslint &&
-code --install-extension eamodio.gitlens &&
-code --install-extension esbenp.prettier-vscode && 
-code --install-extension msjsdiag.debugger-for-chrome &&
-code --install-extension vscode-icons-team.vscode-icons &&
-code --install-extension visualstudioexptteam.vscodeintellicode &&
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension visualstudioexptteam.vscodeintellicode
 
 echo "end"
